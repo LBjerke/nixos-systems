4b5e9e84bd8a2e73a5ef37dcb74b1f8fb987ef26
Your Name <you@example.com>
2022-01-30 14:05:54 -0600

    updating

--- users/orlyn/home.nix
+++ users/orlyn/home.nix
@@ -9,6 +9,10 @@
   home.username = "orlyn";
   home.homeDirectory = "/home/orlyn";
   services.network-manager-applet.enable = true;
+  home.packages = [
+  pkgs.tmux
+
+  ];
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
